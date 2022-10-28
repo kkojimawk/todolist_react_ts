518caa203904603f24c0c6a7b6c2fda9746afd27
kkojimawk <kkojimawk@gmail.com>
2022-10-28 10:12:02 +0900

    todolist作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,131 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    if (!inputText) {
+      return;
+    }
+
+    //新しいTodo作成
+    const newTodo: Todo = {
+      inputValue: inputText,
+      id: todos.length,
+      checked: false,
+    };
+
+    setTodos([newTodo, ...todos]);
+    console.log(inputText);
+    setInputText(inputText);
+  };
+
+  const handleEdit = (id: number, inputValue: string) => {
+    /* ディープコピー(完全にコピーされた別の配列)に変えよう(ミューテートから守るため) */
+    const deepCopy = todos.map((todo) => ({ ...todo }));
+    console.log(deepCopy);
+
+    const newTodos = deepCopy.map((todo) => {
+      if (todo.id === id) {
+        todo.inputValue = inputValue;
+      }
+      return todo;
+    });
+
+    // /* シャローコピー */
+    // const newTodos = todos.map((todo) => {
+    //   if (todo.id === id) {
+    //     todo.inputValue = inputValue;
+    //   }
+    //   return todo;
+    // });
+
+    setTodos(newTodos);
+  };
+
+  const handleChecked = (id: number, checked: boolean) => {
+    /* ディープコピー(完全にコピーされた別の配列)に変えよう(ミューテートから守るため) */
+    const deepCopy = todos.map((todo) => ({ ...todo }));
+    // console.log(deepCopy);
+
+    const newTodos = deepCopy.map((todo) => {
+      if (todo.id === id) {
+        //反転
+        todo.checked = !checked;
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
+
+  //削除
+  const handleDelete = (id: number) => {
+    //idが正しくないのは残す。正しいと消す。
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
 
   return (
     <div className="App">
+      <div>
+        <h2>Todolist with TypeScript</h2>
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          <input
+            type="text"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            className="inputText"
+          />
+          <input type="submit" value="作成" className="submitButton" />
+        </form>
+        <ul className="todoList">
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="text"
+                onChange={(e) => {
+                  handleEdit(todo.id, e.target.value);
+                }}
+                value={todo.inputValue}
+                className="inputText"
+                disabled={todo.checked}
+              />
+              <input
+                type="checkbox"
+                onClick={() => {
+                  handleChecked(todo.id, todo.checked);
+                }}
+                className="inputText"
+              />
+              <button onClick={() => handleDelete(todo.id)}>消</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
